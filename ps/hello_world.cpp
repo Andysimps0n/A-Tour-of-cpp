29e7ff86c4a6866f78920d0f5282365b414c7869
Andy Kim <26eckim@wonhago.org>
2025-08-09 16:52:21 +0900

    reference

---
+++ ps/hello_world.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+//11 15
+
+
+int main(){
+    int a, b;
+    cin >> a;
+    cin >> b;
+    
+    cout << 2*b - a << "\n";
+    
+        
+    return 0;
+        
+    
+}
