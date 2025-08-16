6f8a55243c486eff0636b64953dba97991f168a7
Andy Kim <26eckim@wonhago.org>
2025-08-16 20:51:39 +0900

    8/16

---
+++ Chapter2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+ 
+int main() { 
+    struct Person {
+        string name = "hi";  // member
+        int age;      // member
+    };
+ 
+ 
+   return 0;
+}
--- ps/hello_world.cpp
+++ ps/hello_world.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 using namespace std;
-//11 15
-
 
 int main(){
-    int a, b;
-    cin >> a;
-    cin >> b;
-    cout << 2*b - a << "\n";
+    int t;
+    
+    cin >> t;
+    
+    
+    for (int i=0; i < t; i++) {
+        int r, e, c;
+        cin >> r >> e >> c;
+        if ((e - c) > r) {
+            cout << "advertise" << '\n';
+        } else if ((e - c) == r) {
+            cout << "does not matter" << '\n';
+        } else if ((e-c) < r) {
+            cout << "do not advertise" << '\n';
+        }
+    }
+
+
     return 0;
 }
 
